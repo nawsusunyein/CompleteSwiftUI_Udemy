4cbb46be6df268ddfc9e45ba88b9423314bbb2d9
Naw Su Su Nyein <nawsusunyein@gmail.com>
2022-11-02 14:09:01 +0630

    Initialize new files and folders

---
+++ CompleteSwiftUI_Udemy/CompleteSwiftUI_Udemy/CompleteSwiftUI_UdemyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompleteSwiftUI_UdemyApp.swift
+//  CompleteSwiftUI_Udemy
+//
+//  Created by Naw Su Su Nyein on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CompleteSwiftUI_UdemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
